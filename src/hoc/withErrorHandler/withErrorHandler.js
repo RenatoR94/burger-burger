d7efa1e8e28d3743956528bdf2e7cdf2b5a78ec6
Renato <rrodr646@fiu.edu>
2021-02-05 15:56:09 -0500

    first commit

---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+import Modal from '../../components/UI/Modal/Modal';
+import Auxiliary from '../Auxiliary/Auxiliary';
+
+const withErrorHandler = (WrappedComponent, axios) => {
+  return class extends Component {
+    state = {
+      error: null
+    };
+
+    componentDidMount() {
+      // do this in constructor
+      this.reqInterceptor = axios.interceptors.request.use((req) => {
+        this.setState({ error: null });
+        return req;
+      });
+      this.resInterceptor = axios.interceptors.response.use(
+        (res) => res,
+        (error) => {
+          this.setState({ error: error });
+        }
+      );
+    }
+
+    // ensure removing interceptor (clean up) so if we reuse 'withErrorHandler'
+    // we don't create more and more interceptors with the old ones living on
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
+    }
+
+    errorConfirmedHandler = () => {
+      this.setState({ error: null });
+    };
+
+    render() {
+      return (
+        <Auxiliary>
+          <Modal
+            show={this.state.error}
+            modalClosed={this.errorConfirmedHandler}
+          >
+            {this.state.error ? this.state.error.message : null}
+          </Modal>
+          <WrappedComponent {...this.props} />
+        </Auxiliary>
+      );
+    }
+  };
+};
+
+export default withErrorHandler;
